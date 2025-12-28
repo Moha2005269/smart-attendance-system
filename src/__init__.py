b06a5fe4d82e2698e163f875cde2a402f02dd427
Mhmad hassn Alhmood <m7md0093044@gmail.com>
2025-12-29 01:17:17 +0800

    Remove import of FaceEncoder from __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,3 @@
-from .encoder.face_encoder import FaceEncoder
 from .recognizer.face_recognition_system import FaceRecognitionSystem
 from .logger.csv_logger import CSVLogger
 from .utils.draw_box_label import draw_box_label   
